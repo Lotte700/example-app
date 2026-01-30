7a7e7c990e68c947b9c3d8163feda46bcebdde30
Lotte007 <139039799+Lotte700@users.noreply.github.com>
2026-01-31 04:04:40 +0700

    boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\URL;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -14,11 +15,10 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+public function boot(): void
+{
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 }
